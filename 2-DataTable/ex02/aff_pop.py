f17dadf260a08d17e6421535e76ab2edf550334c
Germán Fredes <germanfredes1988@protonmail.com>
2026-06-09 14:58:17 +0200

    Add ex02 of 2-DataTable

---
+++ 2-DataTable/ex02/aff_pop.py
@@ -0,0 +1,64 @@
+from load_csv import load
+from matplotlib import pyplot as plt
+
+
+def parse_population_data(dataset, country):
+    '''
+    Converts population values to floatsfrom strings with suffixes \
+        (e.g., "M" for million, "k" for thousand)
+    '''
+    try:
+        country_data = dataset[dataset['country'] == country]
+        pop_per_year = country_data.values[0][1:-50]
+        for i, data in enumerate(pop_per_year):
+            if data.endswith("M"):
+                pop_per_year[i] = float(data[:-1]) * 1_000_000
+            elif data.endswith("k"):
+                pop_per_year[i] = float(data[:-1]) * 1_000
+            else:
+                pop_per_year[i] = float(data)
+        return pop_per_year
+
+    except Exception as e:
+        print(f"Error parsing population data for {country}: {e}")
+        return None
+
+
+def population_vs(dataset, country):
+    '''
+    Displays a graph of the population for Spain vs the specified \
+    country in the dataset.
+    '''
+    try:
+        years = range(1800, 2051)
+        spain_population = parse_population_data(dataset, 'Spain')
+        country_population = parse_population_data(dataset, country)
+
+
+        plt.title("Population Projections")
+        plt.plot(years, spain_population, label="Spain")
+        plt.plot(years, country_population, label=country)
+        plt.xlabel("Year")
+        plt.ylabel("Population")
+        plt.xticks(range(1800, 2050, 40))
+        plt.ylim(0, 80_000_000)
+        plt.yticks([20_000_000, 40_000_000, 60_000_000], labels=["20M", "40M", "60M"])
+        plt.legend()
+        plt.tight_layout()
+        plt.show()
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+def main():
+    '''Main function to load the dataset and display the graph.'''
+    dataset = load("population_total.csv")
+    if dataset is not None:
+        population_vs(dataset, "France")
+        return 0
+    return 1
+
+
+if __name__ == "__main__":
+    main()
