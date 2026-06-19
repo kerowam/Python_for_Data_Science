32c686c7901ba42bb77d8186294b79dddaa95948
Germán Fredes <germanfredes1988@protonmail.com>
2026-06-19 17:50:27 +0200

    Change import and add parse of "B"

--- 2-DataTable/ex02/aff_pop.py
+++ 2-DataTable/ex02/aff_pop.py
@@ -1,4 +1,4 @@
-from load_csv import load
+import pandas as pd
 from matplotlib import pyplot as plt
 
 
@@ -11,7 +11,9 @@ def parse_population_data(dataset, country):
         country_data = dataset[dataset['country'] == country]
         pop_per_year = country_data.values[0][1:-50]
         for i, data in enumerate(pop_per_year):
-            if data.endswith("M"):
+            if data.endswith("B"):
+                pop_per_year[i] = float(data[:-1]) * 1_000_000_000
+            elif data.endswith("M"):
                 pop_per_year[i] = float(data[:-1]) * 1_000_000
             elif data.endswith("k"):
                 pop_per_year[i] = float(data[:-1]) * 1_000
@@ -33,6 +35,7 @@ def population_vs(dataset, country):
         years = range(1800, 2051)
         spain_population = parse_population_data(dataset, 'Spain')
         country_population = parse_population_data(dataset, country)
+        max_y = max(max(spain_population), max(country_population))
 
 
         plt.title("Population Projections")
@@ -41,7 +44,7 @@ def population_vs(dataset, country):
         plt.xlabel("Year")
         plt.ylabel("Population")
         plt.xticks(range(1800, 2050, 40))
-        plt.ylim(0, 80_000_000)
+        plt.ylim(0, max_y + 10_000_000)
         plt.yticks([20_000_000, 40_000_000, 60_000_000], labels=["20M", "40M", "60M"])
         plt.legend()
         plt.tight_layout()
@@ -53,7 +56,7 @@ def population_vs(dataset, country):
 
 def main():
     '''Main function to load the dataset and display the graph.'''
-    dataset = load("population_total.csv")
+    dataset = pd.read_csv("population_total.csv")
     if dataset is not None:
         population_vs(dataset, "France")
         return 0
